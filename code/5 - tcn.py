a7d18bd0e1d5089b81057a51ceb573679f8044a1
Sylvain Barthélémy <sylbarth@gmail.com>
2024-11-12 23:39:09 +0100

    tcn

--- code/5 - tcn.py
+++ code/5 - tcn.py
@@ -1,10 +1,11 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.optimizers import Adam
-import matplotlib.pyplot as plt
+
 from sklearn.metrics import mean_squared_error
 
+import tcn
 
 np.random.seed(1)
 tf.random.set_seed(1)
@@ -29,31 +30,37 @@ print("X.shape", X.shape, "y.shape", y.shape)
 print(X)
 print(y)
 
+# model = tf.keras.Sequential([
+#     tf.keras.layers.LSTM(64, input_shape=(T, 1)),
+#     tf.keras.layers.Dense(1)
+# ])
+
 model = tf.keras.Sequential([
-    tf.keras.layers.LSTM(64, input_shape=(T, 1)),
-    tf.keras.layers.Dense(1)
+    tcn.TCN(input_shape=(T, 1),
+        kernel_size=2,
+        use_skip_connections=False,
+        use_batch_norm=False,
+        use_weight_norm=False,
+        use_layer_norm=False
+        ),
+    tf.keras.layers.Dense(1, activation='linear')
 ])
 
 # # Compilation du modèle
-model.compile(loss='mse', optimizer=Adam(learning_rate=0.05))
+model.summary()
+model.compile(loss='mse', optimizer=tf.keras.optimizers.Adam(learning_rate=0.05))
 
 
 # # train du modèle sur 20 epochs
-r = model.fit(X[:-N//2], y[:-N//2], batch_size=32, epochs=20, validation_data=(X[-N//2:], y[-N//2:]), verbose=1)
-
-plt.plot(r.history["loss"], label="loss")
-plt.plot(r.history["val_loss"], label="val_loss")
-plt.legend()
-plt.show()
+r = model.fit(X[:-N//2], y[:-N//2], batch_size=32, epochs=30, validation_data=(X[-N//2:], y[-N//2:]), verbose=1)
 
 # One-step forecast using true targets
 p = model.predict(X)
-# p = p[:,0]
 
 print("MSE", mean_squared_error(y, p))
 
 plt.plot(y, label="y")
-plt.plot(p, label="yhat")
-plt.title("LSTM Predictions")
+plt.plot(p, label="p")
+plt.title("TCN Predictions")
 plt.legend(['actual', 'predicted'])
 plt.show()
